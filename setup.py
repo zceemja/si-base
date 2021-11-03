fe5ed07c246c39d395e6c89fe619e23401d8a852
Mindaugas Jarmolovičius <min.jarmolovicius.17@ucl.ac.uk>
2021-11-03 13:36:50 +0200

    Improved packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup, find_packages
 
+from sibase import __version__
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name='sibase',
-    version='0.1.3',
+    version=__version__,
     description='A basic python package that converts numerical strings with units to base units',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/zceemja/si-base',
     author='Mindaugas Jarmolovičius',
     author_email='zceemja@ucl.ac.uk',
     packages=find_packages(),
     test_suite='tests',
     license_files=('LICENSE',),
     classifiers=[
+        "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
--- sibase/__init__.py
+++ sibase/__init__.py
@@ -1,1 +1,4 @@
+__version__ = "0.1.4"
+__author__ = 'Mindaugas Jarmolovičius'
+
 from .units import Value, Unit, to_base
